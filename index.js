21c08081dea6bf0ae992249fb9c86ba21648b1e7
Coding <codingmaniac227@gmail.com>
2025-09-06 16:59:19 -0400

    feat(static): expose public and images directories

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import router from './route.js'
 const app = express()
 const PORT = 8080
 
-app.use(express.static('public'))
-app.use(express.static('images'))
+app.use('/public', express.static('public'))
+app.use('images', express.static('images'))
 
 app.get('/', (req, res) => {
     res.send('Hello Express')
