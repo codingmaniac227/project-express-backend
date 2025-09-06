c8dc07777f1eb0f40514a46b161845ad8bc50549
Coding <codingmaniac227@gmail.com>
2025-09-06 15:42:55 -0400

    feat(logging): add global lifecycle logger with res.on('finish')

--- index.js
+++ index.js
@@ -6,34 +6,18 @@ const app = express()
 
 const PORT = 8080
 
-app.use('/welcome', (req, res, next) => {
-    console.log(`A new request received at`+Date.now())
+app.use((req,res,next) => {
+    console.log('Start')
+
+    res.on('finish', () => {
+        console.log('End')
+    })
     next()
 })
 
 app.get('/', (req, res) => {
+    console.log('Middle')
      res.send('Hello, Express')
-})
-
-app.get('/welcome', (req, res) => {
-     res.send('Welcome to Express App')
-})
-
-app.use('/user', router)
-
-app.use(express.json())
-
-
-app.get('/things/:name/:id', (req,res) => {
-    const { name, id } = req.params
-    res.json({
-        message: `ID - ${id}; Name - ${name}`
-    })
-})
-
-// Catch all invalid routes
-app.use((req,res) => {
-    res.status(404).type('text').send('Internal Server Error')
 })
 
 app.listen(PORT, () => {
