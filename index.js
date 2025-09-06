31e8f762b3ee48bb3bfa024051159d3ea35d2e4e
Coding <codingmaniac227@gmail.com>
2025-09-06 15:25:52 -0400

    feat(middleware): add route-scoped logger for /welcome

--- index.js
+++ index.js
@@ -6,24 +6,34 @@ const app = express()
 
 const PORT = 8080
 
+app.use('/welcome', (req, res, next) => {
+    console.log(`A new request received at`+Date.now())
+    next()
+})
 
 app.get('/', (req, res) => {
      res.send('Hello, Express')
+})
+
+app.get('/welcome', (req, res) => {
+     res.send('Welcome to Express App')
 })
 
 app.use('/user', router)
 
 app.use(express.json())
 
-app.get('/', (req,res) => {
-    res.send('Hello, Express')
-})
 
 app.get('/things/:name/:id', (req,res) => {
     const { name, id } = req.params
     res.json({
         message: `ID - ${id}; Name - ${name}`
     })
+})
+
+// Catch all invalid routes
+app.use((req,res) => {
+    res.status(404).type('text').send('Internal Server Error')
 })
 
 app.listen(PORT, () => {
