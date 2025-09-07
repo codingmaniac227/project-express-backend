9da7268f443c05e0e275ff95d54b645a90d9eda3
Coding <codingmaniac227@gmail.com>
2025-09-06 23:37:07 -0400

    feat(upload): add Multer for multipart form handling and /form endpoint\ - Configure Multer to parse multipart form data\ - Add POST form handler that logs the req body and req file\\ - Aware of route-scouping and type limits with Multer

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 import express from 'express'
 import router from './route.js'
+import multer from 'multer'
 
 
 const app = express()
+const upload = multer()
 const PORT = 8080
 
-app.use('/public', express.static('public'))
-app.use('images', express.static('images'))
+//app.use(express.urlencoded({extended:true}))
+app.use(upload.single('image'))
+
 
 app.get('/', (req, res) => {
     res.send('Hello Express')
+})
+
+
+app.post('/form', (req, res) => {
+    console.log(req.body)
+    console.log(req.file)
+
+    res.send('Form Received')
 })
 
 
