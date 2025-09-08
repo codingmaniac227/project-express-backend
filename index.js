0dc5173c49f566d1687bc17fae9e1c89ca0d087d
Coding <codingmaniac227@gmail.com>
2025-09-07 21:53:09 -0400

    feat(db,api): connect MongoDB and add Person create + lookup endpoints

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import { connectDB } from './config/db.js'
+import { Person } from './models/Person.js'
 
 const app = express()
 const PORT = 8080
@@ -8,10 +9,34 @@ const MONGODB_URI = 'mongodb+srv://codingmaniac:coding123@cluster0.jwxuvsr.mongo
 
 await connectDB()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('Hello Express')
 })
 
+// Saving Data in MongoDB
+app.post('/person', express.json(), async (req, res) => {
+    const { email, name, age } = req.body
+    const newPerson = new Person({
+        name,
+        age, 
+        email
+    })
+    await newPerson.save()
+    console.log(newPerson)
+    res.send('Person Added')
+})
+
+// Creating Data in MongoDB
+app.put('/person', express.json(), async (req, res) => {
+    const { email } = req.body
+
+    const personData = await Person.find({email})
+
+    console.log(personData)
+    res.send('Person Added')
+})
 
 app.listen(PORT, () => {
     console.log(`Server now running on http://localhost:${PORT}`)
